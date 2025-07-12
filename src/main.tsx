98136276b3a38f3a3cdd6a655163f16b77bfb57b
farisfian06 <farisfian06@gmail.com>
2025-07-13 01:57:17 +0700

    feat: add QueryClientProvider for react-query support

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./style.css";
 import { ToastContainer } from "react-toastify";
 import Routes from "./route/Routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <>
-    <ToastContainer />
-    <Routes />
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer />
+      <Routes />
+    </QueryClientProvider>
   </>
 );
