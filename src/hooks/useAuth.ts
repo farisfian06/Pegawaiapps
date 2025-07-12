4823261ffaac0342c119496d08271a15808bff50
farisfian06 <farisfian06@gmail.com>
2025-07-13 01:57:35 +0700

    feat(hooks): add useDebounce hook and enhance useAuth logout

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import type { User } from "../types/user.types";
+import axiosInstance from "../api/axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const useAuth = () => {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const userData = localStorage.getItem("authUser");
@@ -14,15 +19,35 @@ export const useAuth = () => {
     setLoading(false);
   }, []);
 
-  const logout = () => {
-    localStorage.removeItem("authUser");
-    setUser(null);
-    window.location.reload();
+  const logout = async () => {
+    setIsLoggingOut(true);
+    try {
+      await axiosInstance.post("/logout");
+
+      localStorage.removeItem("authUser");
+      localStorage.removeItem("token");
+      setUser(null);
+
+      // Redirect atau reload
+      navigate("/login");
+    } catch (error) {
+      toast.error(`Logout error ${error}`, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+    } finally {
+      setIsLoggingOut(false);
+    }
   };
 
   return {
     user,
     loading,
     logout,
+    isLoggingOut,
   };
 };
